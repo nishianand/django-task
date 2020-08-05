64f04fbfa61649881e96d6618aff410d2d2493c4
nishianand <nishianand08@gmail.com>
2020-08-06 01:51:52 +0530

    fourth commit

--- myproject/webapp/models.py
+++ myproject/webapp/models.py
@@ -5,10 +5,10 @@ class teacher(models.Model):
     firstname = models.CharField( max_length=20 )
     lastname = models.CharField( max_length=20 )
     teacher_id = models.IntegerField(max_length=3)
-    mobile = models.IntegerField(max_length=10)
+    mobile = models.IntegerField(max_length=12)
 class student(models.Model):
     firstname = models.CharField( max_length=20 )
     lastname = models.CharField( max_length=20 )
     student_id = models.IntegerField(max_length=3)
-    mobile = models.IntegerField(max_length=10)
+    mobile = models.IntegerField(max_length=12)
 
